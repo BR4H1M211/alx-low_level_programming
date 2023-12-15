acaa34a17e628f89899190571c371ea46017881d
Brahim <ebrahim1234567az@gmail.com>
2023-12-15 13:00:27 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,21 +10,18 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	int num;
-
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
-
-	putchar('\n');
-
-	return (0);
+int main() {
+    int digit;
+  
+    for (digit = 0; digit < 10; digit++) {
+        putchar(digit + '0');
+        
+        if (digit != 9) {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    
+    putchar('\n');
+    return 0;
 }
